1b5361ad960b3a52f0ed046a26673c32e67be55d
Patrik Holub <23bulohp@gmail.com>
2024-04-23 14:28:42 +0200

    Adding a markdown file for all plugins currently used Added oil.nvim to replace netrw

--- init.lua
+++ init.lua
@@ -7,8 +7,8 @@ vim.g.have_nerd_font = true
 
 require 'options'
 require 'keymaps'
-require 'lazy-bootstrap'
-require 'lazy-plugins'
+require 'lazy-boot'
+require 'plugins'
 
 -- The line beneath this is called `modeline`. See `:help modeline`
 -- vim: ts=2 sts=2 sw=2 et
